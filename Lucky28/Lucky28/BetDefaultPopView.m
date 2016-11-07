98f61051ccf7663d644c00a9272ff1a29449265a
lindepeng <1013612718@qq.com>
2016-11-07 20:11:21 +0800

    10.07

---
+++ Lucky28/Lucky28/BetDefaultPopView.m
@@ -0,0 +1,25 @@
+//
+//  BetDefaultPopView.m
+//  Lucky28
+//
+//  Created by 九旺网络 on 2016/11/2.
+//  Copyright © 2016年 Jiuwang. All rights reserved.
+//
+
+#import "BetDefaultPopView.h"
+
+@implementation BetDefaultPopView
+- (instancetype)initWithFrame:(CGRect)frame{
+    
+    if (self =[super initWithFrame:frame]) {
+        self.backgroundColor =JWColorA(104, 104, 104, 0.5)
+        
+    }
+    return self;
+}
+
+- (void)show{
+    [self ];
+}
+
+@end
---
+++ Lucky28/Lucky28/JWBetDefaultPopViewController.m
@@ -0,0 +1,37 @@
+//
+//  JWBetDefaultPopViewController.m
+//  Lucky28
+//
+//  Created by 九旺网络 on 2016/11/2.
+//  Copyright © 2016年 Jiuwang. All rights reserved.
+//
+
+#import "JWBetDefaultPopViewController.h"
+
+@interface JWBetDefaultPopViewController ()
+
+@end
+
+@implementation JWBetDefaultPopViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
